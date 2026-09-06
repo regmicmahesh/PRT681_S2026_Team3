a54824876e249f536a49aba283199d23053c705a
badmiguel <badayos.miguel@gmail.com>
2026-09-07 08:40:49 +0930

    feat: add previous week tasks
    ps. previously a developer, moving to tester

---
+++ developer/MiguelBadayos_S365958/README.md
@@ -0,0 +1,3 @@
+these are all the weekly tasks i did before the creation of the new groupings... [original source](https://github.com/regmicmahesh/PRT681_S2026_developers/tree/main/MiguelBadayos_S365958)
+
+moving forward i will be doing testing
---
+++ developer/MiguelBadayos_S365958/Week_5/AuthenticationSample/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using AuthenticationSample.Models;
+
+namespace AuthenticationSample.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [Authorize]
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
